141f25f8f00f55edb5c944e51dcf716c41618173
kiradust <kiradust@gmail.com>
2016-08-31 23:21:48 +0200

    includes pump rate as compartment parameter

--- compartment.py
+++ compartment.py
@@ -2,14 +2,14 @@
 """
 Created on Mon Aug 22 2016
 Python 3.x targeted
-@author: Chris Currin & Kira Dusterwalt
+@author: Chris Currin & Kira Dusterwald
 """
 import numpy as np
 import copy
 from constants import F, R
 from common import default_radius, default_length, \
     clo, ko, nao, \
-    p, pkcc, \
+    default_p, pkcc, \
     gk, gna, gcl, \
     ck, cna
 from sim_time import TimeMixin, Time
@@ -22,7 +22,7 @@ class Compartment(TimeMixin):
     """
 
     def __init__(self, name, radius=default_radius, length=default_length, pkcc2=0, z=-1, nai=140e-3, ki=2.5e-3,
-                 cli=78.3931e-3):
+                 cli=78.3931e-3,p=default_p):
         self.name = name
         self.r = radius  # in um
         self.L = length  # in um
@@ -48,11 +48,13 @@ class Compartment(TimeMixin):
         self.ko = ko
         self.clo = clo
         # define step attributes for t=0
-
+        
+        # define constant element of pump rate
+        self.p = p
         # voltage
         self.V = self.FinvCAr * (self.nai + (self.ki + (self.z * self.xi) - self.cli))
         # pump rate
-        self.jp = p * (self.nai / nao) ** 3
+        self.jp = self.p * (self.nai / nao) ** 3
 
         # register component with simulator
         simulator.Simulator.get_instance().register_compartment(self)
@@ -74,7 +76,7 @@ class Compartment(TimeMixin):
         # update voltage
         self.V = self.FinvCAr * (self.nai + self.ki - self.cli + self.z * self.xi)
         # update cubic pump rate (dependent on sodium gradient)
-        self.jp = p * (self.nai / self.nao) ** 3
+        self.jp = self.p * (self.nai / self.nao) ** 3
         # kcc2
         self.jkcc2 = (gk * self.pkcc2 * (self.ki * self.clo - self.ki * self.cli))  # Fraser and Huang
         # jkcc2=sw*gk*pkcc*(K[ctr-2]-Cl[ctr-2])/10000.0 #Doyon
@@ -104,7 +106,7 @@ class Compartment(TimeMixin):
         self.L = self.w / (np.pi * self.r ** 2)
 
     def copy(self, name):
-        comp = Compartment(name, radius=self.r, length=self.L, pkcc2=self.pkcc2, z=self.z, nai=self.nai, ki=self.ki, cli=self.cli)
+        comp = Compartment(name, radius=self.r, length=self.L, pkcc2=self.pkcc2, z=self.z, nai=self.nai, ki=self.ki, cli=self.cli, p=self.p)
         return comp
 
     def __getitem__(self, item):
